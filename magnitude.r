594151a73caccf1ca56e2eddb2235ca776b47d5b
Nicholas Jones <nicholasj898@gmail.com>
2023-02-27 21:15:55 -0800

    Create magnitude.r

---
+++ magnitude.r
@@ -0,0 +1,14 @@
+#Calculate the magnitude of vector (1, 2, 5) (magnitude = sqrt(1^2+2^2+5^2))
+sqrt(x) #1.000000 1.414214 2.236068
+x^2 #1 4 25
+x[1:3]
+sqrt((x[1]^2) + (x[2]^2) + (x[3]^2))
+#Normalize the vector
+#Calculate the magnitude of the normalized vector
+#Calculate the mean of the 1st 100 numbers: 1,2, ... 100
+
+Preg <- c(2, 3, 2, 1, 2)
+BP <- c(74, 58, 58, 54, 70)
+
+-log(Preg)
+-log(BP)
